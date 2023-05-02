3c95fe56f9567c93de7297747d1918b67cabb957
luckych8080 <luckych8080@gmail.com>
2023-05-02 23:07:24 +0530

    mongoose error stuck

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
 import express, { Express } from "express";
-import mongoose from "mongoose";
+import mongoose, { ConnectOptions } from "mongoose";
 import cors from "cors";
 import morgan from "morgan";
 import router from "./routes/todo";
@@ -8,20 +8,20 @@ const app: Express = express();
 
 const PORT: string | number = 8080;
 
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors());
-app.use(morgan("dev"));
 app.use(router);
 
 const url: string = "mongodb://127.0.0.1:27017/todots";
 
 mongoose
-  .connect(url)
-  .then(() =>
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    })
-  )
+  .connect(url, { useNewUrlParser: true } as ConnectOptions)
+  .then(() => console.log("Connected to MongoDB"))
   .catch((error) => {
     throw error;
   });
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
